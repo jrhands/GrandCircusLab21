63013ba1121d1cdfd26d94f719574c4bd2e3f7b7
Jacob Hands <jrhands@gmail.com>
2018-08-15 16:49:09 -0400

    First commit for lab 22

--- Lab21/Lab21/Models/AddUserViewModel.cs
+++ Lab21/Lab21/Models/AddUserViewModel.cs
@@ -2,13 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace Lab21.Models
 {
     public class AddUserViewModel
     {
+        [Display(Name = "First Name (Required)")]
+        [Required]
+        [MinLength(2)]
         public string FirstName { get; set; }
+
+        [Display(Name = "Last Name (Required)")]
+        [Required]
+        [MinLength(2)]
         public string LastName { get; set; }
+
+        [Display(Name = "Email (Required)")]
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Display(Name = "Phone Number")]
+        [Phone]
+        public string PhoneNum { get; set; }
+
+        [Display(Name = "Zip Code")]
+        public string ZipCode { get; set; }
     }
 }
